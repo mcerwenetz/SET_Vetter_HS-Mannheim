e467969dee1601176de7409435b052c0a79a25a2
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2019-10-19 20:47:04 +0200

    added garbage

--- old/software/apps/praktikum1/vector.h
+++ old/software/apps/praktikum1/vector.h
@@ -6,43 +6,23 @@ class Vector{
 public:
 	Vector(const double& x=0, const double& y=0, const double& z=0);
 	double getX()
-	{
+	{ a
 		return m_x;
 	}
-	double getY()
+	double getY()d
 	{
 		return m_y;
-	}
-	double getZ()
-	{
-		return m_z;
-	}
-	void add (const Vector& v)
-	{
-		this.m_x+=v.m_x;
+	}s d
 		this.m_y+=v.m_y;
-		this.m_z+=v.m_z;
+		this.m_z+=v.m_z;ajknfca sc ascac
 	}
 	void sub (const Vector& v)
 	{
 		this.m_x-=v.m_x;
 		this.m_y-=v.m_y;
-		this.m_z-=v.m_z;
-	}
-	double dot (const Vector& v) const
-	{
-		return((this.x*v.x)+(this.y*v.y)+(this.y*v.y));
-	}
-	Vector cross(const Vector& v) const
-	{
-		Vector result& = new Vector;
-		result.x=((this.y*v.z)-(this.z*v.y));
-		result.y=((this.z*v.x)-(this.x*v.z));
-		result.z=((this.x*v.y)+(this.y*v.x));
-		return result;
-	}
+		this.m_z-=v.m_z;d
 	void print() const
-	{
+	{ a
 		printf("%lf \n", m_x;)
 		printf("%lf \n", m_y;)
 		printf("%lf \n", m_z;)
