6f9f843475a1e9b2a3a95fafc9e97ac8764b4dad
Marius Cerwenetz <1824006@stud.hs-mannheim.de>
2019-10-19 20:23:31 +0200

    implemented getX

---
+++ old/software/apps/praktikum1/vector.h
@@ -0,0 +1,20 @@
+class Vector{
+	double m_x;
+	double m_y;
+	double m_z;
+	
+public:
+	Vector(const double& x=0, const double& y=0, const double& z=0);
+	double getX()
+	{
+		return m_x;
+	}
+	double getY();
+	double getZ();
+	void add (const Vector& v);
+	void sub (const Vector& v);
+	double dot (const Vector& v) const;
+	Vector cross(const Vector& v) const;
+	void print() const;
+}
+	
